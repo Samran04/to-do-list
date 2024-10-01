0433797b6b039be40c0ead102f455fb131ff3878
M.Samran_04 <muhammadsamran04@gmail.com>
2024-10-01 19:29:10 +0530

    load task function upadated js filrf

--- script.js
+++ script.js
@@ -50,4 +50,38 @@ function saveTasks() {
     localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 
+function loadTasks(){
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []; 
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.textContent = task.text; 
+        if (task.completed) {
+            li.classList.add('completed'); 
+        }
+
+                const completeBtn = document.createElement('button');
+                completeBtn.textContent = '✔️'; 
+                completeBtn.addEventListener('click', () => {
+                    li.classList.toggle('completed'); 
+                    saveTasks(); 
+                });
+        
+                
+                const deleteBtn = document.createElement('button');
+                deleteBtn.textContent = 'Delete'; 
+                deleteBtn.className = 'delete-btn'; 
+                deleteBtn.addEventListener('click', () => {
+                    taskList.removeChild(li); 
+                    saveTasks(); 
+                });
+        
+                
+                li.appendChild(completeBtn);
+                li.appendChild(deleteBtn);
+                taskList.appendChild(li); 
+            });
+        }
+        
+
+
 
